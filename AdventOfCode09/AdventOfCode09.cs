e0dfd683bf699bca9c15be87fbcb836673db163c
Marc Zuber <marc.zuber@digitecgalaxus.ch>
2023-01-10 17:55:12 +0100

    09 & 10

--- AdventOfCode09/AdventOfCode09.cs
+++ AdventOfCode09/AdventOfCode09.cs
@@ -2,8 +2,89 @@
 
 public class AdventOfCode09
 {
+    private static Knot head = new(0, 0);
+    private static Knot knot1 = new(0, 0);
+    private static Knot knot2 = new(0, 0);
+    private static Knot knot3 = new(0, 0);
+    private static Knot knot4 = new(0, 0);
+    private static Knot knot5 = new(0, 0);
+    private static Knot knot6 = new(0, 0);
+    private static Knot knot7 = new(0, 0);
+    private static Knot knot8 = new(0, 0);
+    private static Knot tail = new(0, 0);
+
+    private static Knot[] knots = { head, knot1, knot2, knot3, knot4, knot5, knot6, knot7, knot8, tail };
+
+    private static HashSet<(int x, int y)> visited = new();
+
     public static void Main()
     {
-        
+        foreach (var line in File.ReadLines(@"/Users/marc/Playground/AdventOfCode/AdventOfCode09/Inputs.txt"))
+        {
+            var direction = line.Split(" ")[0];
+            var numSteps = int.Parse(line.Split(" ")[1]);
+            
+            for (var i = 0; i < numSteps; i++)
+            {
+                switch (direction)
+                {
+                    case "L":
+                        head.x--;
+                        break;
+                    case "R":
+                        head.x++;
+                        break;
+                    case "U":
+                        head.y--;
+                        break;
+                    case "D":
+                        head.y++;
+                        break;
+                }
+
+                for (var j = 1; j < knots.Length; j++)
+                {
+                    var otherKnot = knots[j - 1];
+                    var thisKnot = knots[j];
+                    
+                    MoveKnot(thisKnot, otherKnot);
+                }
+                
+                if (!visited.Contains((tail.x, tail.y)))
+                {
+                    visited.Add((tail.x, tail.y));
+                }
+            }
+        }
+
+        Console.WriteLine(visited.Count + " Places visited");
+    }
+
+    private static void MoveKnot(Knot thisKnot, Knot otherKnot)
+    {
+        var deltaX = otherKnot.x - thisKnot.x;
+        var deltaY = otherKnot.y - thisKnot.y;
+
+        if (Math.Abs(deltaX) > 1 || (Math.Abs(deltaX) == 1 && Math.Abs(deltaY) > 1))
+        {
+            thisKnot.x += Math.Sign(deltaX);
+        }
+
+        if (Math.Abs(deltaY) > 1|| (Math.Abs(deltaY) == 1 && Math.Abs(deltaX) > 1))
+        {
+            thisKnot.y += Math.Sign(deltaY);
+        }
+    }
+}
+
+public class Knot
+{
+    public int x;
+    public int y;
+    
+    public Knot(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
     }
 }
---
+++ AdventOfCode10/AdventOfCode10.cs
@@ -0,0 +1,78 @@
+﻿namespace AdventOfCode10;
+
+public class AdventOfCode10
+{
+    public static void Main()
+    {
+        var cycle = 0;
+        var value = 1;
+        var sum = 0;
+
+        var reader = File.ReadLines(@"/Users/marc/Playground/AdventOfCode/AdventOfCode10/Inputs.txt").GetEnumerator();
+        var finished = false;
+        var commandExecuted = true;
+        var command = "";
+        var commandRunningForCycles = 0;
+        var outputLine = "";
+
+        do
+        {
+            cycle++;
+
+            if (commandExecuted)
+            {
+                if (reader.MoveNext())
+                {
+                    command = reader.Current;
+                    commandRunningForCycles = 0;
+                    commandExecuted = false;
+                }
+                else
+                {
+                    finished = true;
+                }
+            }
+            
+            if (value >= ((cycle - 1) % 40) - 1 && value <= ((cycle - 1) % 40) + 1)
+            {
+                outputLine += "#";
+            }
+            else
+            {
+                outputLine += ".";
+            }
+
+            if ((cycle == 20 || ((cycle - 20) % 40 == 0 && cycle > 20)))
+            {
+                sum += value * cycle;
+                //Console.WriteLine("Cycle: " + cycle + ", command: " + command + ", running for: " +
+                //                  commandRunningForCycles + ", Value: " + value);
+            }
+
+            if (cycle % 40 == 0)
+            {
+                Console.WriteLine(outputLine);
+                outputLine = "";
+            }
+
+            if (command.Split(" ")[0] == "addx")
+            {
+                if (commandRunningForCycles == 1)
+                {
+                    commandExecuted = true;
+                    value += int.Parse(command.Split(" ")[1]);
+                }
+                else
+                {
+                    commandRunningForCycles++;
+                }
+            }
+            else if (command.Split(" ")[0] == "noop")
+            {
+                commandExecuted = true;
+            }
+        } while (!finished);
+
+        Console.WriteLine(sum);
+    }
+}
